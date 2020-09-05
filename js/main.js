fabc9b0dde021d8144bd932501fc73b5a6cce383
Onan Thairu <thairuonan@gmail.com>
2020-09-05 16:36:06 +0300

    Added a legend

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.pn
 var breaks = [-Infinity, 399, 642, 933.2, 2089.8, Infinity];
 var colors = ["#fef0d9", "#fdcc8a", "#fc8d59", "#e34a33", "#b30000"];
 
+/* Function to choose color */
 function getColor(d) {
     for(var i = 0; i < breaks.length; i++) {
         if(d > breaks[i] && d <= breaks[i+1]) {
@@ -16,6 +17,7 @@ function getColor(d) {
     }
 }
 
+/* Function to style map features */
 function style(feature) {
     return {
         fillColor: getColor(feature.properties.pop_2015),
@@ -27,10 +29,12 @@ function style(feature) {
 }
 
 var geojson;
+/* Function to add (,) to population numbers in the thousands */
 function formatNumber(num) {
     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
 }
 
+/* Adding popups on each feature */
 $.getJSON("./data/towns.geojson", function(data) {
     geojson = L.geoJSON(data, {
         onEachFeature: function(feature, layer) {
@@ -44,3 +48,19 @@ $.getJSON("./data/towns.geojson", function(data) {
         style: style
     }).addTo(map)
 })
+
+/* Adding a legend to the map */
+var legend = L.control({position:"topright"});
+legend.onAdd = function(map) {
+    var div = L.DomUtil.create("div", "legend");
+    div.innerHTML = 
+        '<b>Population in 2015</b><br />by Town<br />' +
+        '<small>Persons/Town</small><br />';
+    for(var i = breaks.length-1; i > 0; i--) {
+        div.innerHTML +=
+        '<i style="background-color: ' + colors[i-1] + '"></i>' +
+        breaks[i-1] + ' - ' + breaks[i] + '<br />';
+    }
+    return div;
+}
+legend.addTo(map);
